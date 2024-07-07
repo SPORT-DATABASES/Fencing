0c922ce39e286b6b1124f6bb734fcfc8a11bc4de
kennymcmillan <kennymcmillan29@gmail.com>
2024-07-07 13:13:08 +0300

    team eurof

--- mySQL_update.py
+++ mySQL_update.py
@@ -4,7 +4,6 @@ import pandas as pd
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 # Database connection details
@@ -59,6 +58,46 @@ with engine.connect() as connection:
 # Use df.to_sql to insert data
 df.to_sql('EuroF_Individ_Ranks', con=engine, if_exists='append', index=False)
 print('Data inserted successfully for Eurofencing.')
+
+########### Insert Eurofencing Team Rankings into the database
+
+# Load the CSV data
+
+df = pd.read_csv('Eurofencing_Team_Rankings.csv')
+
+with engine.connect() as connection:
+    connection.execute(text('''
+    CREATE TABLE IF NOT EXISTS EuroF_Team_Ranks (
+        `id` INT AUTO_INCREMENT PRIMARY KEY,
+        `Comp_1` VARCHAR(255),
+        `Comp_2` VARCHAR(255),
+        `Comp_3` VARCHAR(255),
+        `Comp_4` VARCHAR(255),
+        `Comp_5` VARCHAR(255),
+        `Comp_6` VARCHAR(255),
+        `Comp_7` VARCHAR(255),
+        `Comp_8` VARCHAR(255),
+        `Comp_9` VARCHAR(255),
+        `Comp_10` VARCHAR(255),
+        `Comp_11` VARCHAR(255),
+        `Comp_12` VARCHAR(255),
+        `Gender` VARCHAR(50),
+        `Name` VARCHAR(255),
+        `Nationality` VARCHAR(255),
+        `Points` FLOAT,
+        `Ranking` INT,
+        `Weapon` VARCHAR(50),
+        `Scrape_Date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        `last_updated` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )
+    '''))
+
+    # Truncate the table to remove all existing data
+    connection.execute(text('TRUNCATE TABLE EuroF_Team_Ranks'))
+
+# Use df.to_sql to insert data
+df.to_sql('EuroF_Team_Ranks', con=engine, if_exists='append', index=False)
+print('Data inserted successfully for EuroF_Team_Ranks.')
 
 ################ Now inserting FIE Rankings into Database
 
